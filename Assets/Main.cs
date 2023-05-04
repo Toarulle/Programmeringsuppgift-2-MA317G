8e3859d223fabd92c42b928a020448d6a705a1b4
Toarulle <rallen@live.se>
2023-05-04 19:08:13 +0200

    More work done
    Lots of Matrix programming and general trying to figure out rotation and scale

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -18,17 +18,31 @@ public class Main : MonoBehaviour
     [Range(0,5f)]public float cubeSize = 1f;
     [Range(0,1f)]public float Time = 0.0f;
 
-    private Vector3 currentTransform;
-    private Vector3 targetTransform;
+    public bool doATransforming;
+    public bool doBTransforming;
+    public bool doAScaling;
+    public bool doBScaling;
+    public bool doARotation;
+    public bool doBRotation;
+    
+    public Vector3 aTargetTransform;
+    public Vector3 bTargetTransform;
+    public Vector3 cTransform;
 
-    private Vector3 currentScale;
-    private Vector3 targetScale;
+    private Vector3 aTargetScale;
+    private Vector3 bTargetScale;
+    private Vector3 cScale;
 
-    private Quaternion currentRotation;
-    private Quaternion targetRotation;
+    private Quaternion aTargetRotation;
+    private Quaternion bTargetRotation;
+    private Quaternion cRotation;
+
+    private Vector4 actualTransform;
     
     [SerializeField, HideInInspector]internal Matrix4x4 A;
     [SerializeField, HideInInspector]internal Matrix4x4 B;
+    
+    [SerializeField, HideInInspector]internal Matrix4x4 C;
 
     private void OnEnable()
     {
@@ -37,15 +51,48 @@ public class Main : MonoBehaviour
     
     void Update()
     {
+        aTargetTransform = GetTransformFromMatrix(A);
+        bTargetTransform = GetTransformFromMatrix(B);
+        
+        //lerp-transform the vectors.
+        cTransform = MyVectorLerp(aTargetTransform, bTargetTransform, Time);
+        
+        SetTransformInMatrix(C, cTransform);
+        C[0,3] = cTransform.x;
+        C[1,3] = cTransform.y;
+        C[2,3] = cTransform.z;
+        
+        actualTransform = C * (new Vector4(1f, 1f, 1f, 1f));
+        Debug.Log(actualTransform);
+        Vector3 test = -actualTransform;
 
-        targetTransform = new Vector3(B.m03, B.m13, B.m23);
-        currentTransform = (1.0f - Time) * transform.position + Time * targetTransform;
-        
-        
         using (vectors.Begin())
         {
-            DrawCube(currentTransform, vectors);
+            vectors.Draw(test, test+ new Vector3(A[0,1],A[0,2],A[0,3]),Color.blue);
+            vectors.Draw(test, test+ new Vector3(A[1,1],A[1,2],A[1,3]),Color.blue);
+            vectors.Draw(test, test+ new Vector3(A[2,1],A[2,2],A[2,3]),Color.blue);
+            DrawCube(cTransform, vectors);
         }
+    }
+
+    public void SetTransformInMatrix(Matrix4x4 x, Vector3 v)
+    {
+        x[0,3] = v.x;
+        //Debug.Log(C.m03 + " - " + x.m03 + " - " + v.x);
+        x[1,3] = v.y;
+        x[2,3] = v.z;
+    }
+    public Vector3 GetTransformFromMatrix(Matrix4x4 m)
+    {
+        return new Vector3(m.m03, m.m13, m.m23);
+    }
+    
+    //Egenbyggd Lerpfunktion för vektorer
+    private static Vector3 MyVectorLerp(Vector3 a, Vector3 b, float t)
+    {
+        Vector3 c;
+        c = (1.0f - t) * a + t * b;
+        return c;
     }
     
     private void DrawCube(Vector3 position, VectorRenderer vectors)
@@ -77,6 +124,41 @@ public class MainEditor : Editor
     {
         var main = target as Main;
         if (!main) return;
+        
+        EditorGUI.BeginChangeCheck();
+
+        Vector3 bTransform = main.GetTransformFromMatrix(main.B); 
+        
+        var newBTarget = Handles.PositionHandle(bTransform, main.transform.rotation);
+        
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(main, "Moved Target");
+            var copy = main.B;
+            copy.m03 = newBTarget.x;
+            copy.m13 = newBTarget.y;
+            copy.m23 = newBTarget.z;
+            main.B = copy;
+            EditorUtility.SetDirty(main);
+        }
+        
+        EditorGUI.BeginChangeCheck();
+        
+        Vector3 aTransform = main.GetTransformFromMatrix(main.A);
+
+        var newATarget = Handles.PositionHandle(aTransform, main.transform.rotation);
+        
+        
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(main, "Moved Target");
+            var copy = main.A;
+            copy.m03 = newATarget.x;
+            copy.m13 = newATarget.y;
+            copy.m23 = newATarget.z;
+            main.A = copy;
+            EditorUtility.SetDirty(main);
+        }
     }
 
     public override void OnInspectorGUI()
@@ -114,7 +196,9 @@ public class MainEditor : Editor
             main.A = resultA;
             EditorUtility.SetDirty(main);
         }
+        
         EditorGUILayout.Space(10);
+        EditorGUI.BeginChangeCheck();
         /////////////////////////////////
         //Produce B Matrix in Inspector//
         /////////////////////////////////
@@ -140,6 +224,37 @@ public class MainEditor : Editor
             Undo.RecordObject(main, "Change Matrix B");
             main.B = resultB;
             EditorUtility.SetDirty(main);
+        }        
+        
+        EditorGUILayout.Space(10);
+        EditorGUI.BeginChangeCheck();
+        /////////////////////////////////
+        //Produce C Matrix in Inspector//
+        /////////////////////////////////
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Matrix C (result)");
+        EditorGUILayout.BeginVertical();
+
+        var resultC = Matrix4x4.identity;
+        for (int i = 0; i < 4; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int j = 0; j < 4; j++)
+            {
+                resultC[i, j] = EditorGUILayout.FloatField(main.C[i,j]);
+            }
+            EditorGUILayout.EndHorizontal();
         }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(main, "Change Matrix C");
+            main.C = resultC;
+            EditorUtility.SetDirty(main);
+        }
+        
+        
     }
 }
