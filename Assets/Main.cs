ecbc63a39e48eeaea0be4f054ae8f05e65a436aa
Rasmus Lindberg <a23rasli@student.his.se>
2023-05-05 12:47:07 +0200

    Scale worked on. Small small steps for rotation

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEditor;
@@ -37,50 +38,95 @@ public class Main : MonoBehaviour
     private Quaternion bTargetRotation;
     private Quaternion cRotation;
 
-    private Vector4 actualTransform;
+    private Vector3 baseXVector;
+    private Vector3 baseYVector;
+    private Vector3 baseZVector;
+    
     
     [SerializeField, HideInInspector]internal Matrix4x4 A;
     [SerializeField, HideInInspector]internal Matrix4x4 B;
     
     [SerializeField, HideInInspector]internal Matrix4x4 C;
 
+    private Vector3[] columnVectorAarray;
+    private Vector3[] columnVectorBarray;
+
     private void OnEnable()
     {
         vectors = GetComponent<VectorRenderer>();
+        baseXVector = new Vector3(1, 1, 1);
+        baseYVector = new Vector3(0, 1, 0);
+        baseZVector = new Vector3(0, 0, 1);
     }
     
     void Update()
     {
+        columnVectorAarray = new Vector3[3];
+        columnVectorAarray[0] = new Vector3(A[0, 0], A[1, 0], A[2, 0]);
+        columnVectorAarray[1] = new Vector3(A[0, 1], A[1, 1], A[2, 1]);
+        columnVectorAarray[2] = new Vector3(A[0, 2], A[1, 2], A[2, 2]);
+        
+        columnVectorBarray = new Vector3[3];
+        columnVectorBarray[0] = new Vector3(B[0, 0], B[1, 0], B[2, 0]);
+        columnVectorBarray[1] = new Vector3(B[0, 1], B[1, 1], B[2, 1]);
+        columnVectorBarray[2] = new Vector3(B[0, 2], B[1, 2], B[2, 2]);
+        
         aTargetTransform = GetTransformFromMatrix(A);
         bTargetTransform = GetTransformFromMatrix(B);
-        
-        //lerp-transform the vectors.
-        cTransform = MyVectorLerp(aTargetTransform, bTargetTransform, Time);
-        
-        SetTransformInMatrix(C, cTransform);
-        C[0,3] = cTransform.x;
-        C[1,3] = cTransform.y;
-        C[2,3] = cTransform.z;
-        
-        actualTransform = C * (new Vector4(1f, 1f, 1f, 1f));
-        Debug.Log(actualTransform);
-        Vector3 test = -actualTransform;
 
+        Vector3 tempXA;
+        tempXA.x = Vector3.Dot(columnVectorAarray[0], baseXVector);
+        tempXA.y = Vector3.Dot(columnVectorAarray[1], baseXVector);
+        tempXA.z = Vector3.Dot(columnVectorAarray[2], baseXVector);
+
+        //Debug.Log(tempXA + " - " + new Vector3(A[2,0],A[2,1],A[2,2]) + " - " + baseXVector);
+        Vector3 magnA;
+        magnA.x = Vector3.Magnitude(columnVectorAarray[0]); 
+        magnA.y = Vector3.Magnitude(columnVectorAarray[1]); 
+        magnA.z = Vector3.Magnitude(columnVectorAarray[2]);
+
+        Vector3 magnB;
+        magnB.x = Vector3.Magnitude(columnVectorBarray[0]); 
+        magnB.y = Vector3.Magnitude(columnVectorBarray[1]); 
+        magnB.z = Vector3.Magnitude(columnVectorBarray[2]);
+        
+        
+        ////lerp-transform the vectors and insert into Matrix C////
+        SetTransformInMatrix(ref C, MyVectorLerp(aTargetTransform, bTargetTransform, Time));
+
+        cScale = MyVectorLerp(magnA, magnB, Time);
+        
+        Debug.Log(cScale);
+        
+        cTransform = GetTransformFromMatrix(C);
+        
         using (vectors.Begin())
         {
-            vectors.Draw(test, test+ new Vector3(A[0,1],A[0,2],A[0,3]),Color.blue);
-            vectors.Draw(test, test+ new Vector3(A[1,1],A[1,2],A[1,3]),Color.blue);
-            vectors.Draw(test, test+ new Vector3(A[2,1],A[2,2],A[2,3]),Color.blue);
-            DrawCube(cTransform, vectors);
+            vectors.Draw(Vector3.zero, tempXA, Color.magenta);
+            
+            /*vectors.Draw(Vector3.zero, new Vector3(A[0,0],A[0,1],A[0,2]),Color.blue);
+            vectors.Draw(Vector3.zero, new Vector3(A[1,0],A[1,1],A[1,2]),Color.blue);
+            vectors.Draw(Vector3.zero, new Vector3(A[2,0],A[2,1],A[2,2]),Color.blue);
+            
+            vectors.Draw(Vector3.zero, new Vector3(A[0,0],A[1,0],A[2,0]),Color.magenta);
+            vectors.Draw(Vector3.zero, new Vector3(A[0,1],A[1,1],A[2,1]),Color.magenta);
+            vectors.Draw(Vector3.zero, new Vector3(A[0,2],A[1,2],A[2,2]),Color.magenta);*/
+            DrawCube(cTransform, cScale, vectors);
         }
     }
 
-    public void SetTransformInMatrix(Matrix4x4 x, Vector3 v)
+    public void SetMagnitudeInMatrix(ref Matrix4x4 x, Vector3 m)
     {
-        x[0,3] = v.x;
+        
+    }
+    public void SetTransformInMatrix(ref Matrix4x4 x, Vector3 v)
+    {
+        Matrix4x4 temp = x;
+        temp[0,3] = v.x;
         //Debug.Log(C.m03 + " - " + x.m03 + " - " + v.x);
-        x[1,3] = v.y;
-        x[2,3] = v.z;
+        temp[1,3] = v.y;
+        temp[2,3] = v.z;
+        x = temp;
     }
     public Vector3 GetTransformFromMatrix(Matrix4x4 m)
     {
@@ -95,25 +141,37 @@ public class Main : MonoBehaviour
         return c;
     }
     
-    private void DrawCube(Vector3 position, VectorRenderer vectors)
+    private void DrawCube(Vector3 position, Vector3 size, VectorRenderer vectors)
     {
         //Parallel to X-axis arrows
-        vectors.Draw(position, new Vector3(position.x + cubeSize, position.y, position.z),Color.red);
-        vectors.Draw(position+new Vector3(0,cubeSize,0), new Vector3(position.x + cubeSize, position.y + cubeSize, position.z),Color.red);
-        vectors.Draw(position+new Vector3(0,0,cubeSize), new Vector3(position.x + cubeSize, position.y, position.z + cubeSize),Color.red);
-        vectors.Draw(position+new Vector3(0,cubeSize,cubeSize), new Vector3(position.x + cubeSize, position.y + cubeSize, position.z + cubeSize),Color.red);
+        vectors.Draw(position, 
+            new Vector3(position.x + cubeSize * size.x, position.y, position.z),Color.red);
+        vectors.Draw(position+new Vector3(0,cubeSize * size.y,0), 
+            new Vector3(position.x + cubeSize * size.x, position.y + cubeSize * size.y, position.z),Color.red);
+        vectors.Draw(position+new Vector3(0,0,cubeSize * size.z), 
+            new Vector3(position.x + cubeSize * size.x, position.y, position.z + cubeSize * size.z),Color.red);
+        vectors.Draw(position+new Vector3(0,cubeSize * size.y,cubeSize * size.z), 
+            new Vector3(position.x + cubeSize * size.x, position.y + cubeSize * size.y, position.z + cubeSize * size.z),Color.red);
         
         //Parallel to Y-axis arrows
-        vectors.Draw(position, new Vector3(position.x, position.y + cubeSize, position.z),Color.green);
-        vectors.Draw(position+new Vector3(cubeSize,0,0), new Vector3(position.x + cubeSize, position.y + cubeSize, position.z),Color.green);
-        vectors.Draw(position+new Vector3(0,0,cubeSize), new Vector3(position.x, position.y + cubeSize, position.z + cubeSize),Color.green);
-        vectors.Draw(position+new Vector3(cubeSize,0,cubeSize), new Vector3(position.x + cubeSize, position.y + cubeSize, position.z + cubeSize),Color.green);
+        vectors.Draw(position, 
+            new Vector3(position.x, position.y + cubeSize * size.y, position.z),Color.green);
+        vectors.Draw(position+new Vector3(cubeSize * size.x,0,0), 
+            new Vector3(position.x + cubeSize * size.x, position.y + cubeSize * size.y, position.z),Color.green);
+        vectors.Draw(position+new Vector3(0,0,cubeSize * size.z), 
+            new Vector3(position.x, position.y + cubeSize * size.y, position.z + cubeSize * size.z),Color.green);
+        vectors.Draw(position+new Vector3(cubeSize * size.x,0,cubeSize * size.z), 
+            new Vector3(position.x + cubeSize * size.x, position.y + cubeSize * size.y, position.z + cubeSize * size.z),Color.green);
 
         //Parallel to Z-axis arrows
-        vectors.Draw(position, new Vector3(position.x, position.y, position.z + cubeSize),Color.blue);
-        vectors.Draw(position+new Vector3(cubeSize,0,0), new Vector3(position.x + cubeSize, position.y, position.z + cubeSize),Color.blue);
-        vectors.Draw(position+new Vector3(0,cubeSize,0), new Vector3(position.x, position.y + cubeSize, position.z + cubeSize),Color.blue);
-        vectors.Draw(position+new Vector3(cubeSize,cubeSize,0), new Vector3(position.x + cubeSize, position.y + cubeSize, position.z + cubeSize),Color.blue);
+        vectors.Draw(position, 
+            new Vector3(position.x, position.y, position.z + cubeSize * size.z),Color.blue);
+        vectors.Draw(position+new Vector3(cubeSize * size.x,0,0), 
+            new Vector3(position.x + cubeSize * size.x, position.y, position.z + cubeSize * size.z),Color.blue);
+        vectors.Draw(position+new Vector3(0,cubeSize * size.y,0), 
+            new Vector3(position.x, position.y + cubeSize * size.y, position.z + cubeSize * size.z),Color.blue);
+        vectors.Draw(position+new Vector3(cubeSize * size.x,cubeSize * size.y,0), 
+            new Vector3(position.x + cubeSize * size.x, position.y + cubeSize * size.y, position.z + cubeSize * size.z),Color.blue);
     }
 }
 
@@ -142,13 +200,14 @@ public class MainEditor : Editor
             EditorUtility.SetDirty(main);
         }
         
+        
+        
         EditorGUI.BeginChangeCheck();
         
         Vector3 aTransform = main.GetTransformFromMatrix(main.A);
 
         var newATarget = Handles.PositionHandle(aTransform, main.transform.rotation);
-        
-        
+
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(main, "Moved Target");
